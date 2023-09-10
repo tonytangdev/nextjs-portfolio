ec262289df300a3b63ca9190a732c867a6612021
tonytangdev <tony.tang.developer@gmail.com>
2023-09-10 13:57:34 +0200

    meta tag

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,11 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 
+export const metadata = {
+  title: "Tony Tang - Lead Tech Web Developer in Paris | Web & Mobile Development, Tutoring",
+  description: "Tony Tang is a Paris-based Lead Tech Web Developer specializing in web and mobile development, tutoring, and mentoring. Explore projects and get in touch for collaboration.",
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
